d1ef27a1a2fe08054a0e276c5367fdaf8a3260df
Bundyclan <bundyclantube@gmail.com>
2017-12-09 16:59:10 +0100

    navbar active

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -24,5 +24,35 @@ document.getElementById("btnLogin").addEventListener("click", function(){
     else{
             alert("wrong login details");
     }
-
 });
+
+
+//----------------------------------------------------------------------------------------------------
+
+document.addEventListener('click', function(e){
+            menuNavigation(e);
+        
+        });
+
+function menuNavigation(e){
+    var pageId=e.target.getAttribute('data-page');
+    var navId=e.target.getAttribute('data-nav');
+    if (pageId){
+        var pages = document.querySelectorAll(".page");
+        for (var i=0;i<pages.length;i++){
+            pages[i].style.display="none";
+        }
+        var pageClicked = document.getElementById(pageId);
+        pageClicked.style.display="block";
+    }   
+    
+    if (navId){
+        var navbar = document.querySelectorAll(".nav");
+        for (var i=0;i<navbar.length;i++){
+            navbar[i].classList.remove("nav-active");
+        }
+        var navClicked = document.getElementById(navId);
+        navClicked.classList.add("nav-active");
+        console.log(navClicked);
+    }
+}
